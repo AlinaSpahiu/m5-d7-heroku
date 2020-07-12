eb4c9955d0f5ec41d926ce0fa2f993373e545f85
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-07-12 15:19:12 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const { join } = require("path")
 const listEndpoints = require("express-list-endpoints")
 const helmet = require("helmet")
+const xmlRouter = require("./services/xml")
 
 const booksRouter = require("./services/books")
 const {
@@ -20,27 +21,36 @@ const staticFolderPath = join(__dirname, "../public")
 server.use(express.static(staticFolderPath))
 server.use(express.json())
 
+
+
+ //CORS - Cross Origin Resource Shareing
+
 const whitelist =
   process.env.NODE_ENV === "production"
     ? [process.env.FE_URL]
     : ["http://localhost:3000", "http://localhost:3002"]
 
+
+   
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
+      // allowed
       callback(null, true)
     } else {
+      //not allowed
       callback(new Error("Not allowed by CORS"))
     }
   },
 }
 
-server.use(cors(corsOptions))
+server.use(cors())
 
 server.use(helmet())
 
 //ROUTES
 server.use("/books", booksRouter)
+server.use("/xml", xmlRouter)
 
 // ERROR HANDLERS
 server.use(badRequestHandler)
